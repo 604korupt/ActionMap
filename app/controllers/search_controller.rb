8e26c76f0049bd7661db580b2ed145cc686d6a5d
Brandon Lai <brandonlai@berkeley.edu>
2023-11-20 03:36:25 +0000

    Added invalid search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -7,8 +7,14 @@ class SearchController < ApplicationController
     address = params[:address]
     service = Google::Apis::CivicinfoV2::CivicInfoService.new
     service.key = Rails.application.credentials[:GOOGLE_API_KEY]
-    result = service.representative_info_by_address(address: address)
-    @representatives = Representative.civic_api_to_representative_params(result)
+    begin
+      result = service.representative_info_by_address(address: address)
+      @representatives = Representative.civic_api_to_representative_params(result)
+    rescue StandardError
+      flash[:notice] = "Invalid address! Please enter a valid location."
+      redirect_to representatives_path
+      return
+    end
 
     render 'representatives/search'
   end
