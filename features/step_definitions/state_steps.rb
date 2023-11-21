dcc598eaf917c4c48ae1a0ff2bd36af39381971c
Brandon Lai <brandonlai@berkeley.edu>
2023-11-21 01:07:08 +0000

    39% coverage

--- features/step_definitions/state_steps.rb
+++ features/step_definitions/state_steps.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 # string is symbol here
-Then('I click {string} on state map') do |string|
+Then('I click {string} on the state map') do |string|
   visit "state/#{string}"
+end
+
+Then(/^I click on county (.+)$/) do |county|
+  visit search_representatives_path(county)
 end
